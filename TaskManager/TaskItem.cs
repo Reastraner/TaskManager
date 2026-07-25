752da91e486bc1276a60f2fefdfb3763af08741d
Reastraner <Dayz1235@yandex.ru>
2026-07-25 20:45:47 +0300

    Add task item model

---
+++ TaskManager/TaskItem.cs
@@ -0,0 +1,14 @@
+﻿namespace TaskManager
+{
+    internal class TaskItem
+    {
+        public string Title { get; }
+        public string Description { get; }
+        public bool IsCompleted { get; set; }
+        public TaskItem(string title, string description)
+        {
+            Title = title;
+            Description = description;            
+        }
+    }
+}
