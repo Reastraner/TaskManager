8b6c17744b8118ec13e3f88785bed2c4b64a6325
Reastraner <Dayz1235@yandex.ru>
2026-08-20 12:54:54 +0300

    Add basic WPF task list

---
+++ TaskManager.Desktop/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TaskManager.Desktop
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly TaskService taskService;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            taskService = new TaskService(new MemoryTaskRepository());
+            taskService.AddTask("Первая задача для WPF","Тестовая задача для WPF", TaskPriority.Medium, null);
+
+            TaskList.ItemsSource = taskService.GetTasks();
+        }
+    }
+}
---
+++ TaskManager.Desktop/MemoryTaskRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskManager.Desktop
+{
+    internal class MemoryTaskRepository : ITaskRepository
+    {
+        public TaskData Load()
+        {
+            return new TaskData();
+        }
+       
+        public void Save(TaskData data)
+        {
+
+        }
+    }
+}
